ccdcd52b93f30bc17e4aa9888eb6438f85584ec1
BrendanMyers88 <myersb88@gmail.com>
2018-08-15 22:02:34 -0700

    Titlescreen

--- Tedgame.py
+++ Tedgame.py
@@ -1,25 +1,58 @@
-import sys, pygame
+import pygame
+from pygame.locals import *
 pygame.init()
 
-size = width, height = 800, 600
-speed = [2, 2]
-black = 0, 0, 0
 
-screen = pygame.display.set_mode(size)
+def startscreen():
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption('Gobbo Hunt')
 
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((0, 0, 0))
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+    font = pygame.font.Font(None, 36)
+    text = font.render("Welcome to Gobbo Hunt", 1, (255, 255, 255))
+    textpos = text.get_rect(center=(400, 150))
+    background.blit(text, textpos)
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
-    screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.blit(background, (0, 0))
     pygame.display.flip()
+
+    start = False
+    while not start:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                main()
+
+
+def main():
+    screen = pygame.display.set_mode((800, 600))
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((0, 0, 0))
+
+    # Display some text
+    font = pygame.font.Font(None, 36)
+    text = font.render("Main Game", 1, (255, 255, 255))
+    textpos = text.get_rect(center=(400, 150))
+    background.blit(text, textpos)
+
+    # Blit everything to the screen
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+
+    # Event loop
+    while 1:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
+
+
+if __name__ == '__main__': startscreen()
